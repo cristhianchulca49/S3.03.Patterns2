8041720f2eafe48cf604f13a484a2261278d2dbe
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-22 13:07:50 +0100

    initial commit

---
+++ src/main/java/builder/Dish.java
@@ -0,0 +1,6 @@
+package builder;
+
+public class Dish {
+    String name;
+    String
+}
---
+++ src/main/java/builder/Menu.java
@@ -0,0 +1,10 @@
+package builder;
+
+public class Menu {
+    private String entrant;
+    private String mainCourse;
+    private String dessert;
+    private String coffee;
+    private String drink;
+    
+}
