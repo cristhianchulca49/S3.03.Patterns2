a8c9ab9da83ee23941572ad973149fbe3d3f0938
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-27 11:08:11 +0100

    refactor: remove setters and start attributes from constructor

--- src/main/java/Level1/domain/Menu.java
+++ src/main/java/Level1/domain/Menu.java
@@ -1,6 +1,6 @@
 package Level1.domain;
 
-import Level1.entities.Dish;
+import Level1.domain.entities.Dish;
 
 
 public class Menu {
@@ -9,22 +9,10 @@ public class Menu {
     private Dish complement;
     private String drink;
 
-    Menu() {
-    }
-
-    public void setEntrant(Dish entrant) {
+    public Menu(Dish entrant, Dish mainCourse, Dish complement, String drink) {
         this.entrant = entrant;
-    }
-
-    public void setMainCourse(Dish mainCourse) {
         this.mainCourse = mainCourse;
-    }
-
-    public void setComplements(Dish complement) {
         this.complement = complement;
-    }
-
-    public void setDrink(String drink) {
         this.drink = drink;
     }
 }
