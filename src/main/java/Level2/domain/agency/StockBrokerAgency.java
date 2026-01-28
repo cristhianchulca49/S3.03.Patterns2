3ff1d6f5b86ead996613fc6eebbdc58bb4620840
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-28 17:23:19 +0100

    feat(agency): introduce StockBrokerAgency with state, price handling and notification template

--- src/main/java/Level2/domain/agency/StockBrokerAgency.java
+++ src/main/java/Level2/domain/agency/StockBrokerAgency.java
@@ -3,4 +3,34 @@ package Level2.domain.agency;
 import Level2.domain.observer.Observer;
 
 public class StockBrokerAgency implements Observer {
+
+    private final String name;
+    private StockState state;
+    private double price;
+
+    private static final String TEMPLATE =
+            "%s received notification: Stock market went %s | Current Value: $%.2f";
+
+    public StockBrokerAgency(String name) {
+        this.name = name;
+        this.state = StockState.UNDEFINED;
+    }
+
+    @Override
+    public void update(StockState state, double price) {
+        this.state = state;
+        this.price = price;
+    }
+
+    public String getNotificationMessage() {
+        return String.format(TEMPLATE, name, state.name(), price);
+    }
+
+    public StockState getState() {
+        return state;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/main/java/Level2/domain/agency/StockState.java
+++ src/main/java/Level2/domain/agency/StockState.java
@@ -3,4 +3,5 @@ package Level2.domain.agency;
 public enum StockState {
     UP,
     DOWN,
+    UNDEFINED,
 }
