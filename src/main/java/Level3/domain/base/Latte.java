b09234e2cf9ee9ce00504f77f9a3ece228baa093
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-29 13:03:33 +0100

    feat: create Tea, Match and Latte Bases implementing BubbleTea

---
+++ src/main/java/Level3/domain/base/Latte.java
@@ -0,0 +1,17 @@
+package Level3.domain.base;
+
+import Level3.domain.model.BubbleTea;
+
+import java.math.BigDecimal;
+
+public class Latte implements BubbleTea{
+    @Override
+    public BigDecimal getPrice() {
+        return BigDecimal.valueOf(3.50);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Latte: ";
+    }
+}
