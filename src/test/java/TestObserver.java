767f2c935207af5653e1a3e1e6d2a2f2c6fd1299
cristhianchulca49 <cristhianchulca49@gmail.com>
2026-01-29 10:30:40 +0100

    test: add testStockAgentNotifications to compare the final message

--- src/test/java/TestObserver.java
+++ src/test/java/TestObserver.java
@@ -3,17 +3,29 @@ import Level2.domain.observer.Observer;
 import Level2.domain.stock.StockAgent;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestObserver {
+
+
     @Test
     public void testStockAgentNotifications() {
+
         StockAgent agent = new StockAgent();
         Observer alphaBrokers = new StockBrokerAgency("Alpha Brokers");
-        Observer zenithInvestments = new StockBrokerAgency("Zenith Investments");
 
         agent.addObserver(alphaBrokers);
 
+        agent.updateMarketPrice(150.75);
+        String actualUpMessage = alphaBrokers.toString();
 
-        agent.removeObserver(zenithInvestments);
+        agent.updateMarketPrice(145.50);
+        String actualDownMessage = alphaBrokers.toString();
 
+        String expectedUpMessage = "Alpha Brokers received notification: Stock market went UP | Current Value: $150.75";
+        String expectedDownMessage = "Alpha Brokers received notification: Stock market went DOWN | Current Value: $145.50";
+
+        assertEquals(expectedUpMessage, actualUpMessage);
+        assertEquals(expectedDownMessage, actualDownMessage);
     }
 }
